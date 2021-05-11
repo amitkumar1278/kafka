8c8cc716252d53573ea632fa2b30fdd7ebe9e235
amitkumar12788 <amitkumar12788@gmail.com>
2021-05-12 00:09:53 +0530

    updated producer

--- kafka-beginners/src/main/java/com/github/simpleapp/kafka/tutorial1/ProducerDemoWithCallback.java
+++ kafka-beginners/src/main/java/com/github/simpleapp/kafka/tutorial1/ProducerDemoWithCallback.java
@@ -6,9 +6,10 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.util.Properties;
+import java.util.concurrent.ExecutionException;
 
 public class ProducerDemoWithCallback {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
 
         Logger logger = LoggerFactory.getLogger(ProducerDemoWithCallback.class);
 
@@ -26,9 +27,28 @@ public class ProducerDemoWithCallback {
 
         for(int i = 0; i< 10; i++) {
             // Create a producer record
-            ProducerRecord<String, String> record =
-                    new ProducerRecord<String, String>("first_topic", "hello world " + Integer.toString(i));
+
+            String topic = "first_topic";
+            String value = "hello world" + Integer.toString(i);
+            String key = "id_" + Integer.toString(i);
+
+            ProducerRecord<String, String> record = new ProducerRecord<String, String>(topic, key, value);
 // testing for git
+
+            logger.info("Key: "+ key); // log the key
+            // id_0 is going to partition 1
+            // id_1 is going to partition 0
+            // id_2 is going to partition 2
+            // id_3 is going to partition 0
+            // id_4 is going to partition 2
+            // id_5 is going to partition 2
+            // id_6 is going to partition 0
+            // id_7 is going to partition 2
+            // id_8 is going to partition 1
+            // id_9 is going to partition 2
+
+            // above we observe, Basically by providing the key same key will always go to same partition, run this method multiple times and observe.
+
             // send data -- asynchronous
             producer.send(record, new Callback() {
                 @Override
@@ -45,7 +65,7 @@ public class ProducerDemoWithCallback {
                         logger.error("Error while producing  ", e);
                     }
                 }
-            });
+            }).get(); // block the .send() to make it synchronous, don't do this in productions!
         }
 
         // flush data
