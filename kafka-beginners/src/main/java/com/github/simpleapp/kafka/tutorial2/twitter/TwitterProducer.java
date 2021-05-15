18ee455e2c70f981f7088cc039f31ff8e988fa04
amitkumar12788 <amitkumar12788@gmail.com>
2021-05-16 01:46:46 +0530

    twitter app create setup

---
+++ kafka-beginners/src/main/java/com/github/simpleapp/kafka/tutorial2/twitter/TwitterProducer.java
@@ -0,0 +1,25 @@
+package com.github.simpleapp.kafka.tutorial2.twitter;
+
+public class TwitterProducer {
+
+    public TwitterProducer(){}
+
+    public static void main(String[] args) {
+
+        new TwitterProducer().run();
+
+    }
+
+    public void run(){
+        // Create a twitter client
+
+        // Create a kafka producer
+
+        // loop to send tweets to kafka
+    }
+
+    public  void createTwitterClient(){
+
+    }
+
+}
