2d0b031197c1e07b3bc186bb4c84c6b0d4e2da47
amitkumar12788 <amitkumar12788@gmail.com>
2021-05-25 11:53:25 +0530

    updated twitter producer

--- kafka-beginners/src/main/java/com/github/simpleapp/kafka/tutorial2/twitter/TwitterProducer.java
+++ kafka-beginners/src/main/java/com/github/simpleapp/kafka/tutorial2/twitter/TwitterProducer.java
@@ -32,8 +32,7 @@ public class TwitterProducer {
     String token = "1317060962-ISWgK7uNWhSU6o6uj4lVmCezlybPHbRNqUYUYUYUIY3UZpSY"; // Access Token
     String secret = "587A79du9IHZLwJprGsg5I9PfdXjaDHjAVKOIUYUOYUOUOUOUOmt"; // Access Token Secret
 
-
-    List<String> terms = Lists.newArrayList("bitcoin", "api");
+    List<String> terms = Lists.newArrayList("bitcoin", "usa", "politics", "sport");
 
     public TwitterProducer() {
     }
@@ -131,7 +130,19 @@ public class TwitterProducer {
         properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
         properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
         properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
-        properties.setProperty("", "");
+
+        // create Safe Producer
+        properties.setProperty(ProducerConfig.ENABLE_IDEMPOTENCE_CONFIG, "true");
+        properties.setProperty(ProducerConfig.ACKS_CONFIG, "all");
+        properties.setProperty(ProducerConfig.RETRIES_CONFIG, Integer.toString(Integer.MAX_VALUE));
+        properties.setProperty(ProducerConfig.MAX_IN_FLIGHT_REQUESTS_PER_CONNECTION, "5"); // Kafka 2.0 >= 1.1 so we can keep this as 5, Use 1 otherwise.
+        // End of Safe Producer Config
+
+        // High throughput producer (at the expense of a bit of latency and CPU usage )
+        properties.setProperty(ProducerConfig.COMPRESSION_TYPE_CONFIG, "snappy");
+        properties.setProperty(ProducerConfig.LINGER_MS_CONFIG, "20"); // in milliseconds
+        properties.setProperty(ProducerConfig.BATCH_SIZE_CONFIG, Integer.toString(32*1024)); // 32 KB batch size
+        //properties.setProperty(ProducerConfig., "");
 
         // Create Producer
         KafkaProducer<String, String> producer = new KafkaProducer<String, String>(properties);
